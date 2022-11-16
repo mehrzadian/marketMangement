c5da3c82d2ff4562ff1f2098cc06eabad55c7091
mehrzadian <amirreza.313.mehrzadian@gmail.com>
2022-11-16 10:29:58 +0330

    menu bar

--- src/main/java/ir/ac/kntu/Main.java
+++ src/main/java/ir/ac/kntu/Main.java
@@ -10,6 +10,8 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         int mode = scanner.nextInt();
         String recipe = gettingRecipe(mode);
+        menu();
+        int whatToDisplay = scanner.nextInt();
 
     }
 
@@ -21,5 +23,19 @@ public class Main {
         }
         JsonGenerator jsonGenerator = new JsonGenerator();
         return jsonGenerator.generateBuyRecipe();
+
+
     }
+
+    private static void menu(){
+        System.out.println("What do you want to see? (enter thr number)\n\n" +
+                "1. Displaying the products of a particular bracelet\n" +
+                "2. Sort products by price\n" +
+                "3. Sort products by expiration date\n" +
+                "4. Sort items by number\n" +
+                "5. Show expired products\n" +
+                "6. Display the amount of the total amount of the invoice\n" +
+                "7. Display all goods in the invoice");
+    }
+
 }
