0c2f3d99873295945e40274dd18b597ebc6b156a
mehrzadian <amirreza.313.mehrzadian@gmail.com>
2022-11-16 10:11:25 +0330

    getting recipe

--- src/main/java/ir/ac/kntu/Main.java
+++ src/main/java/ir/ac/kntu/Main.java
@@ -1,8 +1,25 @@
 package ir.ac.kntu;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+
+//        System.out.println(jsonGenerator.generateBuyRecipe());
+        System.out.println("If this is a shopping list enter 1 otherwise enter 0");
+        Scanner scanner = new Scanner(System.in);
+        int mode = scanner.nextInt();
+        String recipe = gettingRecipe(mode);
+
+    }
+
+    private static String gettingRecipe(int mode) {
+        if (mode == 0){
+            System.out.println("Enter your selling recipe");
+        } else {
+            System.out.println("Enter your shopping recipe");
+        }
         JsonGenerator jsonGenerator = new JsonGenerator();
-        System.out.println(jsonGenerator.generateBuyRecipe());
+        return jsonGenerator.generateBuyRecipe();
     }
 }
