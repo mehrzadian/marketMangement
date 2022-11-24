20341e11d3a592eabed34392af9073c99c16c2e8
mehrzadian <amirreza.313.mehrzadian@gmail.com>
2022-11-24 15:02:22 +0330

    menu class created

--- src/main/java/ir/ac/kntu/Main.java
+++ src/main/java/ir/ac/kntu/Main.java
@@ -10,6 +10,7 @@ import java.time.LocalDate;
 
 import static ir.ac.kntu.JsonExtractor.goods;
 import static ir.ac.kntu.JsonExtractor.stuffs;
+import static ir.ac.kntu.Menu.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -25,50 +26,6 @@ public class Main {
         System.out.println(recipe);
 
 
-    }
-
-    private static void whatToDesplay(Scanner scanner, int whatToDisplay, ArrayList<String> stuffs) {
-        if (whatToDisplay == 1) {
-            String kind = scanner.nextLine();
-            displayFromSpecificKind1(stuffs, kind);
-        } else if (whatToDisplay ==2){
-            sortByPrice2(stuffs);
-        } else if (whatToDisplay ==3){
-            sortByDate3(stuffs);
-        } else if (whatToDisplay == 4) {
-            sortByQuantity4(stuffs);
-        } else if (whatToDisplay == 5){
-            printIfExpired5(stuffs);
-        } else if (whatToDisplay == 6){
-            sumOfAll6(stuffs);
-        } else if (whatToDisplay == 7){
-            printAllStuffs7(stuffs);
-        } else{
-            System.out.println(" Unaproppriate number!");
-        }
-    }
-
-    public static String gettingRecipe(int mode) {
-        if (mode == 0){
-            System.out.println("Enter your selling recipe");
-        } else {
-            System.out.println("Enter your shopping recipe");
-        }
-        JsonGenerator jsonGenerator = new JsonGenerator();
-        return jsonGenerator.generateBuyRecipe();
-
-
-    }
-
-    public static void menu(){
-        System.out.println("What do you want to see? (enter thr number)\n\n" +
-                "1. Displaying the products of a particular bracelet\n" +
-                "2. Sort products by price\n" +
-                "3. Sort products by expiration date\n" +
-                "4. Sort items by number\n" +
-                "5. Show expired products\n" +
-                "6. Display the amount of the total amount of the invoice\n" +
-                "7. Display all goods in the invoice");
     }
 
 
--- src/main/java/ir/ac/kntu/Menu.java
+++ src/main/java/ir/ac/kntu/Menu.java
@@ -1,4 +1,53 @@
 package ir.ac.kntu;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Menu {
+
+    public static void whatToDesplay(Scanner scanner, int whatToDisplay, ArrayList<String> stuffs) {
+        if (whatToDisplay == 1) {
+            String kind = scanner.nextLine();
+            displayFromSpecificKind1(stuffs, kind);
+        } else if (whatToDisplay ==2){
+            sortByPrice2(stuffs);
+        } else if (whatToDisplay ==3){
+            sortByDate3(stuffs);
+        } else if (whatToDisplay == 4) {
+            sortByQuantity4(stuffs);
+        } else if (whatToDisplay == 5){
+            printIfExpired5(stuffs);
+        } else if (whatToDisplay == 6){
+            sumOfAll6(stuffs);
+        } else if (whatToDisplay == 7){
+            printAllStuffs7(stuffs);
+        } else{
+            System.out.println(" Unaproppriate number!");
+        }
+    }
+
+    public static String gettingRecipe(int mode) {
+        if (mode == 0){
+            System.out.println("Enter your selling recipe");
+        } else {
+            System.out.println("Enter your shopping recipe");
+        }
+        JsonGenerator jsonGenerator = new JsonGenerator();
+        return jsonGenerator.generateBuyRecipe();
+
+
+    }
+
+    public static void menu(){
+        System.out.println("What do you want to see? (enter thr number)\n\n" +
+                "1. Displaying the products of a particular bracelet\n" +
+                "2. Sort products by price\n" +
+                "3. Sort products by expiration date\n" +
+                "4. Sort items by number\n" +
+                "5. Show expired products\n" +
+                "6. Display the amount of the total amount of the invoice\n" +
+                "7. Display all goods in the invoice");
+    }
+
+
 }
